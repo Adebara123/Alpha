c71596a47b4b31e6e313efa56e34c8dc8de17878
kayzee <olaniyikazeem112@gmail.com>
2022-12-25 02:22:03 +0100

    .

--- src/index.js
+++ src/index.js
@@ -10,8 +10,6 @@ import {
   walletConnectProvider,
 } from "@web3modal/ethereum";
 import { Web3Modal } from "@web3modal/react";
-import { configureChains, createClient, WagmiConfig } from "wagmi";
-import { mainnet, arbitrum, avalanche, bsc, optimism, polygon } from "wagmi/chains";
 
 // --------- React Toastify importation ---------
 import { ToastContainer } from 'react-toastify';
@@ -21,36 +19,49 @@ import './style.css'
 import Home from './views/home'
 import ConnectHome from './views/ConnectDapp/connect'
 
-const chains = [ mainnet, arbitrum, avalanche, bsc, optimism, polygon];
+import '@rainbow-me/rainbowkit/styles.css';
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+} from '@rainbow-me/rainbowkit';
+import { configureChains, createClient, WagmiConfig } from 'wagmi';
+import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
+import { alchemyProvider } from 'wagmi/providers/alchemy';
+import { publicProvider } from 'wagmi/providers/public';
 
-// Wagmi client
-const { provider } = configureChains(chains, [
-  walletConnectProvider({ projectId: process.env.PROJECT_ID }),
-]);
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors: modalConnectors({ appName: "alfa.society", chains }),
-  provider,
+
+const { chains, provider } = configureChains(
+  [mainnet, polygon, optimism, arbitrum],
+  [
+    alchemyProvider({ apiKey: "RcI6kAisXpQhOycB2S4qsvMuxTXZfR3P"}),
+    publicProvider()
+  ]
+);
+
+const { connectors } = getDefaultWallets({
+  appName: 'Alpha',
+  chains
 });
 
-// Web3Modal Ethereum Client
-const ethereumClient = new EthereumClient(wagmiClient, chains);
- 
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider
+})
+
+
 const App = () => {
   return (
     <Router>
       <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
        <ToastContainer />
        <Routes>
          <Route path="/" element={<ConnectHome />} />
          <Route path="/alpha-teleporthq" element={<Home />} />
       </Routes>
+      </RainbowKitProvider>
       </WagmiConfig>
-
-      <Web3Modal
-        projectId="f34a7a9265b0a2846c88cc89d9407ce5"
-        ethereumClient={ethereumClient}
-      />
     </Router>
   )
 }
